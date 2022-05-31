664c63d988c80a4965d12d91886beb823ab81889
VivifyIdeas <vivify@VivifyIdeass-MacBook-Pro-4.local>
2022-05-31 19:37:25 +0200

    zadatak 1

---
+++ app/Http/Controllers/TeamController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Team;
+
+use Illuminate\Http\Request;
+
+class TeamController extends Controller
+{
+    public function index() {
+        $teams = Team::all();
+        return view('teams', compact('teams'));
+
+
+    }
+}
